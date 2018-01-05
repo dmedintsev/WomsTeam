3c08b5523a92b1545a75b14f107b72323cc6c34b
Omelchenko Misha <l2maximum@mail.ru>
2018-01-05 15:18:45 +0300

    Fix soc acc

--- WomsTeam/settings.py
+++ WomsTeam/settings.py
@@ -141,12 +141,12 @@ SITE_ID = 1
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-#STATIC_DIR = os.path.join(BASE_DIR, 'static')
-#STATICFILES_DIRS = [STATIC_DIR, ]
-#STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR, ]
+STATIC_URL = '/static/'
 
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-#MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # allauth facebook
 SOCIALACCOUNT_PROVIDERS = {
